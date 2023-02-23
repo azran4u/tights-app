d624afdbf984dbeff99e290c9090466cea700507
Eyal Azran <azran4u@gmail.com>
2023-02-23 09:48:36 +0200

    revert firebase config

--- src/shared/services/firebase-config.ts
+++ src/shared/services/firebase-config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyBJxoF__P-dUYSkaeglEIDgSgXfrKSbs-g",
   authDomain: "tights-app.firebaseapp.com",
   projectId: "tights-app",
   storageBucket: "tights-app.appspot.com",
   messagingSenderId: "133108001267",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  appId: "1:133108001267:web:a1ae3bce25d972702b8a86",
 };
 
 const app = initializeApp(firebaseConfig);
